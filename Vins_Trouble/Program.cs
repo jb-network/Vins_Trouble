f648433717c21941550ff3d585e47f7b84bcbc41
jb-network <85036487+jb-network@users.noreply.github.com>
2022-06-02 22:10:41 -0500

    Updated comments

--- Vins_Trouble/Program.cs
+++ Vins_Trouble/Program.cs
@@ -1,9 +1,8 @@
 ﻿// This is challenge work for the "C# Players Guide"
-// Level 18 the Vin Fletcher's Arrows Challenge
-// This challenge requires a new arrow class with fields for arrowhead type, fletching type, and length
-// A user should be able to pick the arrowhead, fletching type, and length
-// Once those items are selected a new arrows instance should be created
-// Add a get cost method that returns its cost as a float based on the arrow attributes selected by the user
+// Level 19 the Vin's Trouble Challenge
+// This challenge mirrors the Level 18 challenge, but requires that that Arrow Class is changed from public to private
+// Addtionally a "getter" methods must be added for each of the fields in the class.
+
 
 //Main
 StartMenu();
